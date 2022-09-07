63ade922a632d1f0e36e52db4ef1366289e914cf
Seamus O'Sullivan <info@seamuso.com>
2022-09-07 20:18:15 +0200

    dep

--- pages/api/puppeteer.js
+++ pages/api/puppeteer.js
@@ -1,13 +1,10 @@
-const chromium = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer");
+// const chrome = require("chrome-aws-lambda");
 
 export default async function handler(req, res) {
   try {
-    const browser = await chromium.puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
+    const browser = await puppeteer.launch({
       headless: true,
-      ignoreHTTPSErrors: true,
     });
 
     const aboutBlankPage = (await browser.pages())[0];
