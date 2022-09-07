d8d02cdce767628629cd252d1548d8c586312024
Seamus O'Sullivan <info@seamuso.com>
2022-09-07 23:00:52 +0200

    d

--- pages/api/puppeteer.js
+++ pages/api/puppeteer.js
@@ -1,13 +1,28 @@
-// const puppeteer = require("puppeteer-core");
-const chromium = require("chrome-aws-lambda");
+let chrome = {};
+let puppeteer;
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core");
+} else {
+  puppeteer = require("puppeteer");
+}
 
 export default async function handler(req, res) {
-  try {
-    const browser = await chromium.puppeteer.launch({
-      args: chromium.args,
-      executablePath: await chromium.executablePath,
+  let options = {};
+
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    options = {
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
       headless: true,
-    });
+      ignoreHTTPSErrors: true,
+    };
+  }
+
+  try {
+    const browser = await puppeteer.launch(options);
 
     const aboutBlankPage = (await browser.pages())[0];
     if (aboutBlankPage) {
@@ -36,7 +51,7 @@ export default async function handler(req, res) {
     });
 
     console.log(nasaWeatherDataScrape, "in");
-    res.send(nasaWeatherDataScrape);
+    res.json(nasaWeatherDataScrape);
 
     const newPage = (await browser.pages())[0];
     await newPage.close();
